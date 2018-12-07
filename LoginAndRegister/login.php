1e4f3cd11b8d38acff1d6f5afef4937b154cf863
LinKai945 <linkai945@gmail.com>
2018-12-07 18:59:27 +0800

    优化登录判断逻辑,增加对管理员用户的判别

--- LoginAndRegister/login.php
+++ LoginAndRegister/login.php
@@ -1,14 +1,29 @@
 <?php
-	$uname=$_POST["uname"];
-	$pwd=$_POST["pwd"];
-	include_once("conn.php");
-	$result = mysqli_query($conn,"select *  from Reader where RdId ='$uname' and RdPW='$pwd' ");
-	if(mysqli_num_rows($result) != 0){
-		echo "<script>alert('登录成功')</script>";
-	}else{
-		echo "<script>alert('用户名或密码错误!')</script>";
+	include_once("../ConnectToDatabase/conn.php");
+		$uname = $_POST["uname"];
+		$pwd = $_POST["pwd"];
 
-		//header('location:Login&&Register.php');
-	}
+		$result = mysqli_query($conn, "SELECT RdPW FROM Reader WHERE RdId = $uname");
+		// var_dump(mysqli_num_rows($result));
+		if ( @mysqli_num_rows($result) == 0 ) {
+			$result = mysqli_query($conn, "SELECT AdPW FROM Admins WHERE AdId = $uname");
+			// var_dump(mysqli_num_rows($result));
+		}
+
+		if ( @mysqli_num_rows($result) > 0 ) {
+			$myrow = mysqli_fetch_row($result);
+			if ($pwd == $myrow[0]) {
+				echo "<script>alert('登录成功')</script>";
+				if (substr($uname, 0, 2) == '99') {
+					echo "<script>alert('到管理员主界面')</script>";
+				} else {
+					echo "<script>alert('到用户主界面')</script>";
+				}
+			} else {
+				echo "<script>alert('密码错误！')</script>";
+			}
+		} else {
+			echo "<script>alert('用户名不存在！')</script>";
+		}
 
 ?>
