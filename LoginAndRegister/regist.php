c4784d3174ceb497def163d2ef687794f396d564
LinKai945 <linkai945@gmail.com>
2018-12-23 12:57:49 +0800

    修复bug:禁止普通用户注册管理员账号

--- LoginAndRegister/regist.php
+++ LoginAndRegister/regist.php
@@ -10,23 +10,26 @@
 		$uname = $_POST["uname"];
 		$pwd = $_POST["pwd"];
 		$pwd2 = $_POST["pwd2"];
-
-		if($pwd != $pwd2){
-			echo "<script>alert('两次输入的密码不同,请重新输入！')</script>";
+		if (substr($uname, 0, 2) == '99'){
+			echo "<script>alert('用户名非法！')</script>";
 			echo "<script>location.href='LoginAndRegister.php';</script>";
 		}else{
-			$result = mysqli_query($conn, "SELECT RdId FROM Reader WHERE RdId = $uname");
-			if ( @mysqli_num_rows($result) != 0 ) {
-				echo "<script>alert('用户已存在！')</script>";
+			if($pwd != $pwd2){
+				echo "<script>alert('两次输入的密码不同,请重新输入！')</script>";
 				echo "<script>location.href='LoginAndRegister.php';</script>";
 			}else{
-				$sql = "INSERT INTO Reader VALUES($uname,'default','男',$pwd)";
-				mysqli_query($conn, $sql);
-				echo "<script>alert('注册成功！')</script>";
-				echo "<script>location.href='LoginAndRegister.php';</script>";
+				$result = mysqli_query($conn, "SELECT RdId FROM Reader WHERE RdId = $uname");
+				if ( @mysqli_num_rows($result) != 0 ) {
+					echo "<script>alert('用户已存在！')</script>";
+					echo "<script>location.href='LoginAndRegister.php';</script>";
+				}else{
+					$sql = "INSERT INTO Reader VALUES($uname,'default','男',$pwd)";
+					mysqli_query($conn, $sql);
+					echo "<script>alert('注册成功！')</script>";
+					echo "<script>location.href='LoginAndRegister.php';</script>";
+				}
 			}
 		}
-		
 	?>
 </body>
 </html>
